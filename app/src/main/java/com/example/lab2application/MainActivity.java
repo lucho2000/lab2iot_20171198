6c7706a3348388a1d61b312191d54e772f8a0004
unknown <luis.ramost@pucp.edu.pe>
2023-09-16 18:09:57 -0500

    vistas de login y logica de register, falta obtener datos api

---
+++ app/src/main/java/com/example/lab2application/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.lab2application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //linkeo hacia el registro
+        Button buttonRegistro = findViewById(R.id.button);
+        buttonRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/lab2application/RegisterActivity.java
@@ -0,0 +1,153 @@
+package com.example.lab2application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+        Button buttonRegistro = findViewById(R.id.buttonCreate);
+        buttonRegistro.setEnabled(false);
+
+        TextView textoNombre = findViewById(R.id.nombre);
+        TextView textoApellido =findViewById(R.id.apellido);
+        TextView textoCorreo =findViewById(R.id.correo);
+        TextView textoContrasenia =findViewById(R.id.contrasenia);
+
+        CheckBox deAcuerdo = findViewById(R.id.checkBox);
+
+
+        deAcuerdo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b && !textoNombre.getText().toString().isEmpty() &&
+                        !textoApellido.getText().toString().isEmpty() &&
+                        !textoCorreo.getText().toString().isEmpty() &&
+                        !textoContrasenia.getText().toString().isEmpty()){
+
+                    buttonRegistro.setEnabled(true);
+                }else {
+                    buttonRegistro.setEnabled(false);
+                }
+            }
+        });
+
+
+        textoNombre.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (deAcuerdo.isChecked() && !textoApellido.getText().toString().isEmpty() &&
+                        !textoCorreo.getText().toString().isEmpty() &&
+                        !textoContrasenia.getText().toString().isEmpty()){
+
+                    buttonRegistro.setEnabled(true);
+                }else {
+                    buttonRegistro.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        textoApellido.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (deAcuerdo.isChecked() && !textoNombre.getText().toString().isEmpty() &&
+                        !textoCorreo.getText().toString().isEmpty() &&
+                        !textoContrasenia.getText().toString().isEmpty()){
+
+                    buttonRegistro.setEnabled(true);
+                }else {
+                    buttonRegistro.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+
+        textoCorreo.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (deAcuerdo.isChecked() && !textoNombre.getText().toString().isEmpty() &&
+                        !textoApellido.getText().toString().isEmpty() &&
+                        !textoContrasenia.getText().toString().isEmpty()){
+
+                    buttonRegistro.setEnabled(true);
+                }else {
+                    buttonRegistro.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        textoContrasenia.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (deAcuerdo.isChecked() && !textoNombre.getText().toString().isEmpty() &&
+                        !textoApellido.getText().toString().isEmpty() &&
+                        !textoCorreo.getText().toString().isEmpty()){
+
+                    buttonRegistro.setEnabled(true);
+                }else {
+                    buttonRegistro.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        buttonRegistro.setOnClickListener(view -> {
+            Intent intent = new Intent(RegisterActivity.this , PrincipalActivity.class);
+            startActivity(intent);
+        });
+
+    }
+}
