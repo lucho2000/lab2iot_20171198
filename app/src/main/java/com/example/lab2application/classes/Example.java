62176d591bd26d33d8b5958725ec5b089e05391c
unknown <luis.ramost@pucp.edu.pe>
2023-09-16 23:19:04 -0500

    creacion de clases para api

---
+++ app/src/main/java/com/example/lab2application/classes/Example.java
@@ -0,0 +1,14 @@
+package com.example.lab2application.classes;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Example {
+
+    private List<Result> results;
+
+    private Info info;
+
+    private Map<String, Object> additionalProperties = new LinkedHashMap<String, Object>();
+}
